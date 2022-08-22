ee7c8df913b107a675b4dcd509f4ea3898dcbf34
arieldavis22 <amanichai100@gmail.com>
2022-08-22 18:15:55 -0400

    packages 1

---
+++ src/Packages/Main.java
@@ -0,0 +1,13 @@
+package Packages;
+
+//import javafx.scene.Node;
+
+public class Main {
+    public static void main(String[] args) {
+//        Node node = null;
+//        javafx.scene.Node = null;
+
+        MyWindow myWindow = new MyWindow("Stuff");
+        myWindow.setVisible(true);
+    }
+}
